f58b3847df29f2b04c6cac1ddad9accec5342835
joshdoyle94 <joshdoyle94@gmail.com>
2022-11-30 13:44:44 -0500

    finished teams model and crud capability

--- homework_crud/sports/views.py
+++ homework_crud/sports/views.py
@@ -18,9 +18,29 @@ class TeamsView(APIView):
         serializer = TeamSerializer(teams, many=True)
         return Response({'teams': serializer.data})
 
+    def post(self, request):
+        serializer = TeamSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 class TeamsDetailView(APIView):
     #  View class for teams/:pk
     def get(self, request, pk):
         team = get_object_or_404(Team, pk=pk)
         serializer = TeamSerializer(team)
         return Response({'team': serializer.data})
+
+    def patch(self, request, pk):
+        team = get_object_or_404(Team, pk=pk)
+        serializer = TeamSerializer(team, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        team = get_object_or_404(Team, pk=pk)
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
